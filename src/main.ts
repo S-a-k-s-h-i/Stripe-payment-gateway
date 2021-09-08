94dd294f0059726406974681768180cbf1dbf720
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-09-08 16:30:13 +0530

    'added-helmet-and-cors'

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,24 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  //to enable cross origin requests
+  app.enableCors();
 
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector))
   );
 
+  //using helmet to secure app by setting various http headers
+  app.use(helmet());
+
   // Set base view dir to views in root folder
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
   // Set view engine to hbs(handlebars)
   app.setViewEngine('hbs');
 
